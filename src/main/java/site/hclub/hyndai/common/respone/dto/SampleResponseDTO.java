ed77d8e0a4cb2f7a23231738e848ef8b038937a9
smartpodo@kakao.com <smartpodo@kakao.com>
2024-02-10 22:45:32 +0900

    fix: java configurations & Tomcat log enocoding

--- src/main/java/site/hclub/hyndai/common/respone/dto/SampleResponseDTO.java
+++ src/main/java/site/hclub/hyndai/common/respone/dto/SampleResponseDTO.java
@@ -1,11 +1,12 @@
 package site.hclub.hyndai.common.respone.dto;
-import site.hclub.hyndai.sample.domain.SampleVO;
-import java.util.List;
 
 import lombok.Data;
+import site.hclub.hyndai.domain.SampleVO;
+
+import java.util.List;
 
 
 @Data
 public class SampleResponseDTO {
-	private List<SampleVO> sampleList;
+    private List<SampleVO> sampleList;
 }
